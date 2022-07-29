8def1d59fa80a2e0eb18b13e4a3329f6b463f97d
Wojtek <wojciechdebsqi@gmail.com>
2022-07-29 17:52:21 +0200

    Create a module to connect to Neo4j db

---
+++ neo4j_db/__init__.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+from neo4j_db.Neo4jDB import Neo4jDB
+
+# Loading environment variables from .env file
+load_dotenv()
+NEO4J_URL = os.getenv("NEO4J_URL")
+NEO4J_USERNAME = os.getenv("NEO4J_USERNAME")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+
+neo4j_db = Neo4jDB(NEO4J_URL, NEO4J_USERNAME, NEO4J_PASSWORD)
+
+neo4j_db.close()
