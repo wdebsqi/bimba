83ca7ed9e30c891269ee0600dac5adebe47c2fb7
Wojtek <wojciechdebsqi@gmail.com>
2022-07-29 18:05:30 +0200

    Add pytest fixtures to the tests

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -1,22 +1,19 @@
+import pytest
 from app import flask_app
 
 
 class TestFlask:
-    client = flask_app.test_client()
+    @pytest.fixture(scope="class")
+    def client(self):
+        return flask_app.test_client()
 
-    def test_home(self):
-        res = self.client.get("/")
+    def test_home(self, client):
+        res = client.get("/")
         assert res.status_code == 200
         assert isinstance(res.json, dict)
         print(res.json.get("message"))
         assert res.json.get("message") == "Hello World!"
 
-    def test_service(self):
-        res = self.client.get("/service")
+    def test_service(self, client):
+        res = client.get("/service")
         assert res.status_code == 405
-
-
-if __name__ == "__main__":
-    tester = TestFlask()
-    tester.test_home()
-    tester.test_service()
--- tests/test_neo4j.py
+++ tests/test_neo4j.py
@@ -1,6 +1,14 @@
-from neo4j_db import neo4j_db
+import pytest
+from neo4j_db import NEO4J_USERNAME, NEO4J_URL, NEO4J_PASSWORD
+from neo4j_db.Neo4jDB import Neo4jDB
 
 
 class TestNeo4j:
-    def test_ping(self):
+    @pytest.fixture(scope="class")
+    def neo4j_db(self):
+        db = Neo4jDB(NEO4J_URL, NEO4J_USERNAME, NEO4J_PASSWORD)
+        yield db
+        db.close()
+
+    def test_ping(self, neo4j_db):
         assert neo4j_db.ping() is True
