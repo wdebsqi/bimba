82794a1e7b8988bf34a2fbdf6fb6fa2b0d8eb153
wdebsqi <wojciechdebsqi@gmail.com>
2022-07-29 20:25:54 +0200

    Comment out load_dotenv
    Checking if this is what breaks env variables in GitHub Actions

--- neo4j_db/__init__.py
+++ neo4j_db/__init__.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from neo4j_db.Neo4jDB import Neo4jDB
 
 # Loading environment variables from .env file
-load_dotenv()
+#load_dotenv()
 NEO4J_URL = os.getenv("NEO4J_URL")
 NEO4J_USERNAME = os.getenv("NEO4J_USERNAME")
 NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
