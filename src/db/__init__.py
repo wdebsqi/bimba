db90474213debdc735bc2c8554428d5e32b89d13
Wojtek <wojciechdebsqi@gmail.com>
2022-08-20 21:11:05 +0200

    Fix env variables parsing

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -8,7 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 load_dotenv()
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("POSTGRES_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("POSTGRES_URL").replace("\\", "")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 sql_db = SQLAlchemy(app)
 migrate = Migrate(app, sql_db)
