b6207adb8bb7e7b2a8ce47bb9bab5dbd143e41c7
Wojtek <wojciechdebsqi@gmail.com>
2022-09-03 15:23:21 +0200

    Update tests

--- tests/test_db_logger.py
+++ tests/test_db_logger.py
@@ -1,9 +1,5 @@
-from datetime import datetime
-
 import pytest
 from src.db.DBLogger import DBLogger
-
-TIMESTAMP_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 
 class TestDBLogger:
@@ -12,6 +8,5 @@ class TestDBLogger:
         return DBLogger()
 
     def test_log(self, logger):
-        now = datetime.now().strftime(TIMESTAMP_FORMAT)
-        res = logger.log_message(now, "test message", __file__, "test", False)
+        res = logger.log_message("test message", __file__, "test", False)
         assert res is True
