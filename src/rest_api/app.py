cb96c0666f412453f578678f266064bda7b942e1
Wojtek <wojciechdebsqi@gmail.com>
2022-10-24 21:25:40 +0200

    Add flask-cors to the REST API

--- src/rest_api/app.py
+++ src/rest_api/app.py
@@ -1,7 +1,10 @@
+from flask_cors import CORS
+
 from . import create_app
 
 app = create_app()
 
+cors = CORS(app, resources={r"/stops": {"origins": "*"}})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
