4a4dd21666acb108ec199650538178a13920d0e5
Wojtek <wojciechdebsqi@gmail.com>
2022-11-01 15:32:36 +0100

    Add locations info to REST API

--- src/rest_api/src/ResponseFormatter.py
+++ src/rest_api/src/ResponseFormatter.py
@@ -1,16 +1,24 @@
 ROUTE_STATS_LINES_CHOSEN = "lines_chosen"
 ROUTE_STATS_NUM_OF_CHANGES = "num_of_changes"
 ROUTE_STATS_TOTAL_STOPS = "total_stops"
+STOP_LAT = "stop_lat"
+STOP_LON = "stop_lon"
 STOP_NAME = "stop_name"
+ZONE_ID = "zone_id"
 
 
 class ResponseFormatter:
     """Class responsible for formatting the response returned to the REST API."""
 
-    START_NODE = "start_node"
     END_NODE = "end_node"
+    NAME = "name"
+    LAT = "lat"
     LINE_CHOSEN = "line_chosen"
+    LOCATIONS = "locations"
+    LON = "lon"
+    PATH = "path"
     ROUTE = "route"
+    START_NODE = "start_node"
 
     def format_single_route_response(self, route_data: dict) -> dict:
         """Formats the response from the format:
@@ -23,9 +31,9 @@ class ResponseFormatter:
         ```
         To the more developer-friendly format that can be returned by the REST API."""
 
-        result = {self.ROUTE: []}
+        result = {self.ROUTE: [], self.LOCATIONS: []}
 
-        for i, rel in enumerate(route_data["path"].relationships):
+        for i, rel in enumerate(route_data[self.PATH].relationships):
             start_node = rel.start_node.get(STOP_NAME)
             end_node = rel.end_node.get(STOP_NAME)
             line_chosen = route_data[ROUTE_STATS_LINES_CHOSEN][i]
@@ -36,6 +44,23 @@ class ResponseFormatter:
                     self.LINE_CHOSEN: line_chosen,
                 }
             )
+
+            result[self.LOCATIONS].append(
+                {
+                    self.NAME: start_node,
+                    self.LAT: rel.start_node.get(STOP_LAT),
+                    self.LON: rel.start_node.get(STOP_LON),
+                }
+            )
+
+            if i == len(route_data[self.PATH].relationships) - 1:
+                result[self.LOCATIONS].append(
+                    {
+                        self.NAME: end_node,
+                        self.LAT: rel.end_node.get(STOP_LAT),
+                        self.LON: rel.end_node.get(STOP_LON),
+                    }
+                )
 
         result[ROUTE_STATS_NUM_OF_CHANGES] = route_data[ROUTE_STATS_NUM_OF_CHANGES]
         result[ROUTE_STATS_TOTAL_STOPS] = len(route_data[ROUTE_STATS_LINES_CHOSEN])
