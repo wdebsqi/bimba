8610523dad6a9340d5e68a01d8353fb3938b8aa8
Wojtek <wojciechdebsqi@gmail.com>
2023-06-21 19:43:37 +0200

    fix models calculating timestamp on service start

--- src/db/models/FileProcessingLog.py
+++ src/db/models/FileProcessingLog.py
@@ -9,7 +9,7 @@ class FileProcessingLog(Base, CRUD):
     __tablename__ = "file_processing_logs"
 
     id = Column(Integer, primary_key=True)
-    created_at = Column(DateTime, default=datetime.now(), nullable=False)
+    created_at = Column(DateTime, default=datetime.now, nullable=False)
     file_name = Column(String, nullable=False)
     file_contents_hash = Column(String, nullable=False)
     processed_successfully = Column(Boolean, nullable=False)
--- src/db/models/Request.py
+++ src/db/models/Request.py
@@ -9,7 +9,7 @@ class Request(Base, CRUD):
     __tablename__ = "requests"
 
     id = Column(Integer, primary_key=True)
-    created_at = Column(DateTime, default=datetime.now(), nullable=False)
+    created_at = Column(DateTime, default=datetime.now, nullable=False)
     ip = Column(String, nullable=False)
     uri = Column(String, nullable=False)
     method = Column(String, nullable=False)
